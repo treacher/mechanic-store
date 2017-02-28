b62e2f33f3d451ae2d70a557ef2bb12e6208c5ae
Michael Treacher <m.w.treacher@gmail.com>
2017-02-28 18:27:10 +1100

    Add database interaction for creating companies. Need to abstract this out into another package next.

--- controllers/companies_controller.go
+++ controllers/companies_controller.go
@@ -4,32 +4,41 @@ import (
   "github.com/julienschmidt/httprouter"
   "github.com/treacher/mechanic-store/models"
 
+  "gopkg.in/pg.v5"
+
   "net/http"
   "encoding/json"
-  "math/rand"
   "time"
 )
 
 func CreateCompany(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+  db := pg.Connect(&pg.Options{ User: "postgres", Database: "mechanic-store" })
+
   var company models.Company
 
   if r.Body == nil {
-    http.Error(w, "No body provided", 400)
+    http.Error(w, "No body provided", http.StatusBadRequest)
     return
   }
 
   err := json.NewDecoder(r.Body).Decode(&company)
 
   if err != nil {
-    http.Error(w, err.Error(), 400)
+    http.Error(w, err.Error(), http.StatusBadRequest)
     return
   }
 
   currentTime := time.Now()
 
-  company.Id = rand.Uint64()
   company.CreatedAt = currentTime
   company.UpdatedAt = currentTime
+
+  err = db.Insert(&company)
+
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+    return
+  }
 
   w.WriteHeader(http.StatusCreated)
 
--- controllers/companies_controller_test.go
+++ controllers/companies_controller_test.go
@@ -2,6 +2,7 @@ package controllers_test
 
 import (
   "github.com/treacher/mechanic-store/router"
+  "gopkg.in/DATA-DOG/go-sqlmock.v1"
 
   "net/http"
   "net/http/httptest"
@@ -10,6 +11,16 @@ import (
 )
 
 func TestCreateCompany(t *testing.T) {
+  db, mock, err := sqlmock.New()
+  if err != nil {
+      t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+  }
+  defer db.Close()
+
+  mock.ExpectBegin()
+  mock.ExpectExec("INSERT INTO companies").WithArgs("dennis", "+64505050505", "foo@bar.com")
+  mock.ExpectCommit()
+
   companyJson := `{"name": "dennis", "phone": "+64505050505", "email": "foo@bar.com" }`
   companyReader := strings.NewReader(companyJson)
 
@@ -20,10 +31,25 @@ func TestCreateCompany(t *testing.T) {
   rr := setupRecorder(req)
 
   testResponseCode(t, rr.Code, http.StatusCreated)
+
+  if err := mock.ExpectationsWereMet(); err != nil {
+    t.Errorf("there were unfulfilled expections: %s", err)
+  }
 }
 
 func TestCreateCompanyWithEmptyBody(t *testing.T) {
   req, err := http.NewRequest("POST", "/companies", nil)
+
+  handleFatalError(t, err)
+
+  rr := setupRecorder(req)
+
+  testResponseCode(t, rr.Code, http.StatusBadRequest)
+}
+
+func TestCreateCompanyWithInvalidJSON(t *testing.T) {
+  stringReader := strings.NewReader("foo")
+  req, err := http.NewRequest("POST", "/companies", stringReader)
 
   handleFatalError(t, err)
 
